40df36699bda06d29775746ce3d2f666ca98a103
clarenceXSF <clarence_feng@outlook.com>
2016-04-11 20:16:07 +0800

    完成销售详细的BLL层代码

---
+++ src/EShop.BLL/ISaleDetailManager.cs
@@ -0,0 +1,31 @@
+﻿using EShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.BLL
+{
+    public interface ISaleDetailManager
+    {
+        /// <summary>
+        /// 根据订单id查询订单详细
+        /// </summary>
+        /// <param name="BsId"></param>
+        /// <returns></returns>
+        List<SaleDetailView> FindSaleDetail(string BsId);
+        /// <summary>
+        /// 添加订单详细
+        /// </summary>
+        /// <param name="detail"></param>
+        /// <returns></returns>
+        bool InsertSaleDetail(SaleDetail detail);
+        /// <summary>
+        /// 删除订单详细
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        bool DeleteSaleDetail(string id);
+    }
+}
---
+++ src/EShop.BLL/Manager/SaleDetailManager.cs
@@ -0,0 +1,28 @@
+﻿using EShop.DAL;
+using EShop.DAL.Service;
+using EShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.BLL.Manager
+{
+    public class SaleDetailManager : ISaleDetailManager
+    {
+        private ISaleDetailService _saleDetailService = new SaleDetailService();
+        public List<SaleDetailView> FindSaleDetail(string BsId)
+        {
+            return _saleDetailService.FindSaleDetail(BsId);
+        }
+        public bool InsertSaleDetail(SaleDetail detail)
+        {
+            return _saleDetailService.InsertSaleDetail(detail);
+        }
+        public bool DeleteSaleDetail(string id)
+        {
+            return _saleDetailService.DeleteSaleDetail(id);
+        }
+    }
+}
